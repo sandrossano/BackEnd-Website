69eedbe7c2bf51bd29976643767628515bd530e3
sandrossano <32746792+sandrossano@users.noreply.github.com>
2022-04-12 18:45:35 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json());
+//app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 const querydata = "SELECT * FROM t_pages";
 // Route to get all posts
